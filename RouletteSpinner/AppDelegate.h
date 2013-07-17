846ef2758bf850a4c5aacb764ae018f064241b97
Jared Bergman <jbergman@terpmail.umd.edu>
2013-07-16 20:41:09 -0400

    Initial Commit

---
+++ RouletteSpinner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RouletteSpinner
+//
+//  Created by Jared Bergman on 7/16/13.
+//  Copyright (c) 2013 iD Tech Camps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RouletteSpinner/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  RouletteSpinner
+//
+//  Created by Jared Bergman on 7/16/13.
+//  Copyright (c) 2013 iD Tech Camps. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
