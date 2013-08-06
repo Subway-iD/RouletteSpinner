90d35e5c514f1c9c4e610978f12990e5df53efbd
Jared Bergman <jbergman@terpmail.umd.edu>
2013-08-05 20:30:41 -0400

    first

---
+++ RouletteSpinner/AppController.h
@@ -0,0 +1,13 @@
+//
+//  AppController.h
+//  RouletteSpinner
+//
+//  Created by Jared Bergman on 7/16/13.
+//  Copyright (c) 2013 iD Tech Camps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppController : NSObject
+
+@end
---
+++ RouletteSpinner/AppController.m
@@ -0,0 +1,13 @@
+//
+//  AppController.m
+//  RouletteSpinner
+//
+//  Created by Jared Bergman on 7/16/13.
+//  Copyright (c) 2013 iD Tech Camps. All rights reserved.
+//
+
+#import "AppController.h"
+
+@implementation AppController
+
+@end
